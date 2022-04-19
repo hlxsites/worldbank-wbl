8faa97c1f6d65269e655e91ae3e18a1d87b02d6c
Katie Sexton <ksexton@adobe.com>
2022-04-19 13:52:37 -0600

    feat: footer block

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,10 +1,12 @@
-import { readBlockConfig } from '../../scripts/scripts.js';
+import {
+  readBlockConfig,
+  toClassName,
+} from '../../scripts/scripts.js';
 
 /**
  * loads and decorates the footer
- * @param {Element} block The header block element
+ * @param {Element} block The footer block element
  */
-
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
@@ -14,5 +16,20 @@ export default async function decorate(block) {
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
+
+  footer.querySelectorAll(':scope > div').forEach((section) => {
+    const row = document.createElement('div');
+    row.classList.add('footer-row');
+    row.append(section);
+    footer.append(row);
+    // subsections within sections
+    const subsections = section.querySelectorAll('h2 + ul');
+    if (subsections.length) {
+      subsections.forEach((sub) => {
+        sub.classList.add(`footer-${toClassName(sub.previousElementSibling.textContent)}`);
+        sub.previousElementSibling.remove();
+      });
+    }
+  });
   block.append(footer);
 }
