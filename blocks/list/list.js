9d26c5958eb35b952e9b12195ffbdcd1441a218f
Katie Sexton <ksexton@adobe.com>
2022-04-19 16:42:57 -0600

    fix: bug fixes

--- blocks/list/list.js
+++ blocks/list/list.js
@@ -6,33 +6,41 @@ export default async function decorate(block) {
   const rows = [...block.children];
   rows.forEach((row) => {
     row.classList.add('list-row');
-    const columns = [...row.children];
-    columns.forEach((column) => {
-      column.classList.add('list-col');
-      const prevDetail = column.querySelector('p:first-child + h2, p:first-child + h3');
-      const postDetail = column.querySelector('h2 + p em, h3 + p em');
+    const cols = [...row.children];
+    cols.forEach((col) => {
+      col.classList.add('list-col');
+      const img = col.querySelector('p picture');
+      const pa = col.querySelector('p a');
+      if ((img && pa) && (img.parentElement.nextElementSibling === pa.parentElement)) {
+        const imgWrap = pa.cloneNode(true);
+        imgWrap.innerHTML = img.outerHTML;
+        img.replaceWith(imgWrap);
+        pa.remove();
+      }
+      const prevDetail = col.querySelector('p:first-child + h2, p:first-child + h3');
+      const postDetail = col.querySelector('h2 + p em, h3 + p em');
       if (prevDetail) {
-        const p = column.querySelector('p:first-child');
+        const p = col.querySelector('p:first-child');
         const em = p.querySelector('em');
         if (p.textContent === em.textContent) {
           p.classList.add('detail', 'list-detail');
         }
       }
       if (postDetail) {
-        const p = column.querySelector('h2 + p, h3 + p');
+        const p = col.querySelector('h2 + p, h3 + p');
         const em = p.querySelector('em');
         if (p.textContent === em.textContent) {
           p.classList.add('detail', 'list-detail');
         }
       }
-      const as = column.querySelectorAll('a');
+      const as = col.querySelectorAll('a');
       as.forEach((a) => {
         if (a.parentNode.nodeName === 'DIV') {
           a.classList.add('btn', 'list-btn');
         }
       });
     });
-    if (columns.length === 2) { // two column layout
+    if (cols.length === 2) { // two column layout
       row.classList.add('list-row-2-col');
     }
   });
