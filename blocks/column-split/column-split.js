c3ec1ee9c825bd440f950d58920888e2a520a121
Katie Sexton <ksexton@adobe.com>
2022-04-29 14:31:56 -0600

    fix: section metadata prep

--- blocks/column-split/column-split.js
+++ blocks/column-split/column-split.js
@@ -4,16 +4,21 @@
  */
 export default async function decorate(block) {
   const container = block.parentNode.parentNode;
+  const wrapper = document.createElement('div');
   const split = document.createElement('div');
   split.classList.add('content-wrapper');
   [...container.children].forEach((child) => {
     if (![...child.classList].includes('column-split-wrapper')) {
       split.append(child);
+    } else {
+      wrapper.append(child);
     }
   });
-  container.prepend(split);
-  const h1 = container.querySelector('h1');
+  wrapper.prepend(split);
+  const h1 = wrapper.querySelector('h1');
   if (h1) {
-    container.prepend(h1);
+    wrapper.prepend(h1);
   }
+
+  container.append(wrapper);
 }
